4d4e9523802afd3208df7fb4431022b073c33233
GaidarOS <karvouniaris.b@gmail.com>
2017-02-18 01:15:20 +0100

    Almost completed part4 from week_2

--- rmuast_s17_module_2/exercise_discharge_data/GraphGenerator.py
+++ rmuast_s17_module_2/exercise_discharge_data/GraphGenerator.py
@@ -1,10 +1,11 @@
 # __author__ = ""
 import re
+import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import matplotlib.dates as mdates
 from scipy.interpolate import spline
-import numpy as np 
 
 # Initiate variables
 voltage = []
@@ -32,13 +33,33 @@ for i in range(len(s)):
 # ploting the discharge graph with raw values
 plt.plot(s, voltage)
 plt.title('Battery discarge graph')
-plt.ylabel('Voltage')
-plt.xlabel('Timestamp')
+plt.ylabel('Voltage(V)')
+plt.xlabel('Time')
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
 # plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1)) # it can further space the timestamps
 # plt.gcf().autofmt_xdate() # Autoformating of the dates could use it
 plt.show()
 
+
+soc = np.linspace(100, 10, len(voltage))
+
+fig = plt.figure(1, (7, 4))
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(voltage, soc)
+
+fmt = '%.0f%%'  # Format you want the ticks, e.g. '40%'
+xticks = mtick.FormatStrFormatter(fmt)
+ax.yaxis.set_major_formatter(xticks)
+plt.gca().invert_xaxis()
+plt.title('State of Charge graph')
+plt.ylabel('SoC(%)')
+plt.xlabel('Voltage(V)')
+# function sort of the form of: y= e^(-x^2)-x
+plt.show()
+
+
+"""
+# smooth the voltage graph
 time = np.array(time)
 voltage = np.array(voltage)
 
@@ -46,8 +67,6 @@ xnew = np.linspace(time.min(), time.max(), 10)  # 300 represents number of point
 print(xnew)
 
 power_smooth = spline(time, voltage, xnew)
-
 plt.plot(xnew, power_smooth)
 plt.show()
-"""
 """
